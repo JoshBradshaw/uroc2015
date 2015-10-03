bd9f5db0d37d0c7fb72ee22de4eac27aca2c5f6f
Josh Bradshaw <joshbradshaw11@gmail.com>
2015-10-03 10:12:11 -0400

    cross referenced candidates to scan number

--- read_mgf.py
+++ read_mgf.py
@@ -2,14 +2,14 @@ from pprint import pprint
 import re
 
 def read_peptides(filename):
-	peptides = {}
-	f = open(filename,'rU')
-	for line in f:
-		line = line.strip()
-		key_val_pair = line.split(' ')
-		peptides[key_val_pair[0]] = float(key_val_pair[1])
-	
-	return peptides
+    peptides = {}
+    f = open(filename,'rU')
+    for line in f:
+        line = line.strip()
+        key_val_pair = line.split(' ')
+        peptides[key_val_pair[0]] = float(key_val_pair[1])
+    
+    return peptides
 
 def read_mdf(fp):
     metadata = {}
@@ -27,7 +27,10 @@ def read_mdf(fp):
                 if line.count('=') == 1:
                     label, value = line.split('=')
                     if not label == 'CHARGE':
-                        metadata[label] = float(value)
+                        if '.' in value:
+                            metadata[label] = float(value)
+                        else:
+                            metadata[label] = int(value)
                     else:
                         metadata[label] = value
                 elif line[0].isdigit():
@@ -41,36 +44,36 @@ def read_mdf(fp):
 fn_mass = {'A':71.03711, 'R':156.10111,'N':114.04293,'D':115.02694,'C':160.08919,'E':129.04295,'Q':128.05858,'G':57.02146,'H':137.05891,'I':113.08406,'L':113.08406,'K':128.09496,'M':131.04049,'F':147.06841,'P':97.0256,'S':87.3203,'T':101.04768,'W':186.07931,'Y':163.06333,'V':99.06841}
 
 def calc_charge(metadata):
-	return int(metadata['CHARGE'][0])
+    return int(metadata['CHARGE'][0])
 
 def calc_mass(metadata,charge):
-	pep_mass = metadata['PEPMASS']
-	mass = (pep_mass - 1.007)*charge - 18.01
-	return mass
+    pep_mass = metadata['PEPMASS']
+    mass = (pep_mass - 1.007)*charge - 18.01
+    return mass
 
 def find_candidates(peptides,mass):
-	candidates = []
-	for peptide in peptides:
-		if abs(peptides[peptide] - mass) < 0.5 :
-			candidates.append(peptide)
-	
-	return candidates
+    candidates = []
+    for peptide in peptides:
+        if abs(peptides[peptide] - mass) < 0.5 :
+            candidates.append(peptide)
+    
+    return candidates
 
 
 if __name__ == '__main__':
     spectrums = read_mdf('test.mgf')
     peptides = read_peptides('peptides.txt')
+    candidates = {}
     z_values = []
     mass = []
     count  = 0
     for metadata, spectrum in spectrums:
-	    charge = calc_charge(metadata)
-	    z_values.append(charge)
-	    m = calc_mass(metadata,charge)
-	    mass.append(m)
-	    candidates = find_candidates(peptides,m)
-	    if candidates == []:
-		    count+=1
-    print count
+        charge = calc_charge(metadata)
+        z_values.append(charge)
+        m = calc_mass(metadata,charge)
+        scan_number = metadata['SCANS']
+        mass.append(m)
+        candidates[scan_number] = find_candidates(peptides,m)
+    pprint(candidates)
 
 
