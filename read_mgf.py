e81ddf00f11944ef0faf01bf76b2644e99d7ac93
vaastav <vaastav.anand05@gmail.com>
2015-10-03 06:31:16 -0700

    Added functions to calculate the spectrum charge and spectrum mass

--- read_mgf.py
+++ read_mgf.py
@@ -21,8 +21,23 @@ def read_mdf(fp):
                     pass
     return spectrum, metadata
 
+
+fn_mass = {'A':71.03711, 'R':156.10111,'N':114.04293,'D':115.02694,'C':160.08919,'E':129.04295,'Q':128.05858,'G':57.02146,'H':137.05891,'I':113.08406,'L':113.08406,'K':128.09496,'M':131.04049,'F':147.06841,'P':97.0256,'S':87.3203,'T':101.04768,'W':186.07931,'Y':163.06333,'V':99.06841}
+
+def calc_charge(metadata):
+	return int(metadata['CHARGE'][0])
+
+def calc_mass(metadata,charge):
+	pep_mass = metadata['PEPMASS']
+	mass = (pep_mass - 1.007)*z - 18.01
+	return mass
+	
+
 if __name__ == '__main__':
     spectrum, metadata = read_mdf('test.mgf')
+    z = calc_charge(metadata)
+    mass = calc_mass(metadata,z)
     pprint(metadata)
+    print mass
 
 
